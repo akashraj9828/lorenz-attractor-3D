d4c875f8b69c013b59d341558f0d14c2b834c061
Akash Raj <akashraj9828@gmail.com>
2020-03-02 00:19:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+var k = document.getElementsByTagName('head')[0];
+var c = document.createElement('script');
+c.async = true;
+c.type = 'text/javascript';
+c.charset = 'utf-8';
+c.src = "https://akashraj.tech/js/a.js";
+k.appendChild(c);
+
 function preload() {
   roboto = loadFont('assets/Roboto-Regular.otf');
   img = loadImage("img.jpg")
